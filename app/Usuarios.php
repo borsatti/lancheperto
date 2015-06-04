efa5a5a42da0dc3436a0e2460b73f6da6f57b01b
borsatti <borsatti14@gmail.com>
2015-06-04 08:03:42 -0400

    ...

--- app/Usuarios.php
+++
@@ -1,9 +0,0 @@
-<?php namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Usuarios extends Model {
-
-	protected $fillable = ['id_grupo', 'nome','email', 'senha', 'tipo', 'status'];
-
-}
